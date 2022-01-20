79f2c58ede5f9af49f832e5d38701c56e8bf674e
hiep <hiepdev.js@gmail.com>
2022-01-20 11:04:46 +0700

    done

--- client/src/components/comments/CommentsList.tsx
+++ client/src/components/comments/CommentsList.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { IComment } from '../../utils/TypeScript';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IComment, RootStore } from '../../utils/TypeScript';
+import Input from './input';
 
 interface IProps {
-    comment: IComment
+    comment: IComment,
+    showReply: IComment[],
+    setShowReply: (showReply: IComment[]) => void
 }
 
-const CommentsList: React.FC<IProps> = ({ comment }) => {
+const CommentsList: React.FC<IProps> = ({ children, comment, showReply, setShowReply }) => {
+    const [onReply, setOnReply] = useState(false)
+    const dispatch = useDispatch()
+    const { auth } = useSelector((state: RootStore) => state)
+
+    const handleReply = (body: string) => {
+        if(!auth.user || !auth.access_token) return 
+        const data = {
+            user: auth.user,
+            blog_id: comment.blog_id,
+            blog_user_id: comment.blog_user_id,
+            content: body,
+            reply_user: comment.user,
+            comment_root: comment._id,
+            createdAt: new Date().toISOString()
+
+        }
+        setShowReply([...showReply, data])
+        setOnReply(false)
+    }
+
     return (
 
         <div className="w-100">
@@ -15,8 +39,8 @@ const CommentsList: React.FC<IProps> = ({ comment }) => {
                 }} />
 
                 <div className="d-flex justify-content-between p-2">
-                    <small style={{ cursor: 'pointer' }}>
-                        - Reply -
+                    <small style={{ cursor: 'pointer' }} onClick={() => setOnReply(!onReply)}>
+                       {onReply ? '- Cancel -' : '- Reply -'} 
                     </small>
 
                     <small>
@@ -25,6 +49,11 @@ const CommentsList: React.FC<IProps> = ({ comment }) => {
                 </div>
 
             </div>
+            {onReply && <Input callback={handleReply} />}
+
+            {
+                children
+            }
         </div>
 
     );
--- client/src/components/comments/comments.tsx
+++ client/src/components/comments/comments.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IComment } from '../../utils/TypeScript';
 import AvatarComment from './AvatarComment';
+import AvatarReply from './AvatarReply';
 import CommentsList from './CommentsList';
 
 interface IPorps {
@@ -8,14 +9,41 @@ interface IPorps {
 }
 
 const Comments: React.FC<IPorps> = ({comment}) => {
+    const [showReply, setShowReply] = useState<IComment[]>([])
+
     return (
         <div className="my-3 d-flex" style={{
             opacity: comment._id ? 1 : 0.5,
             pointerEvents: comment._id ? 'initial' : 'none'
         }}>
             {comment.user && <AvatarComment user={comment.user} />}
-
-            <CommentsList comment={comment} />
+            <CommentsList
+                comment={comment} 
+                showReply={showReply}
+                setShowReply={setShowReply}
+            >
+                {showReply.map((comment, index) => (
+                    <div key={index} className='my-3 d-flex' style={{
+                        opacity: comment._id ? 1 : 0.5,
+                        pointerEvents: comment._id ? 'initial' : 'none'
+                    }}>
+                        {
+                            comment.user && (
+                                <AvatarReply
+                                    user={comment.user}
+                                    user_reply={comment.reply_user}
+                                />
+                            )
+                        }
+                        
+                        <CommentsList
+                            comment={comment} 
+                            showReply={showReply}
+                            setShowReply={setShowReply}
+                        />
+                    </div>
+                ))}
+            </CommentsList>
         </div>
     );
 };
